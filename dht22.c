a186ebe1bd20fe61a524a62eac3a4911747d5ed9
Jens Gecius <jens@gecius.de>
2019-02-10 19:59:46 +0100

    Changed logic to 1. collect data, 2. evaluate data; added debug output

--- dht22.c
+++ dht22.c
@@ -3,6 +3,7 @@
  *	Simple test program to test the wiringPi functions
  *	Based on the existing dht11.c
  *	Amended by technion@lolware.net
+ +	Further amended by gmanic
  */
 
 #include <wiringPi.h>
@@ -15,9 +16,12 @@
 
 #include "locking.h"
 
+#define DEBUG false
+
 #define MAXTIMINGS 85
 static int DHTPIN = 7;
 static int dht22_dat[5] = {0,0,0,0,0};
+static uint16_t raw[MAXTIMINGS];
 
 static uint8_t sizecvt(const int read)
 {
@@ -37,37 +41,42 @@ static int read_dht22_dat()
   uint8_t laststate = HIGH;
   uint8_t counter = 0;
   uint8_t j = 0, i;
+  int dht_sum;
 
   dht22_dat[0] = dht22_dat[1] = dht22_dat[2] = dht22_dat[3] = dht22_dat[4] = 0;
 
   // pull pin down for 18 milliseconds
   pinMode(DHTPIN, OUTPUT);
   digitalWrite(DHTPIN, HIGH);
-  delay(500);
+  delay(50);
   digitalWrite(DHTPIN, LOW);
-  delay(20);
-  // prepare to read the pin
+  delay(1);
+  digitalWrite(DHTPIN, HIGH);
+//  delayMicroseconds(15);
+// prepare to read the pin
   pinMode(DHTPIN, INPUT);
 
   // detect change and read data
   for ( i=0; i< MAXTIMINGS; i++) {
-    counter = 0;
+    raw[i] = 0;
     while (sizecvt(digitalRead(DHTPIN)) == laststate) {
-      counter++;
-      delayMicroseconds(2);
-      if (counter == 255) {
-        break;
-      }
+      raw[i]++;
+      delayMicroseconds(1);
+      if (raw[i] == 10000) break;
     }
     laststate = sizecvt(digitalRead(DHTPIN));
+    if (raw[i] == 10000) break;
+  }
 
-    if (counter == 255) break;
+  for ( i=0; i< MAXTIMINGS; i++) {
 
-    // ignore first 3 transitions
+    if (DEBUG) printf("Counter for transition %d: %d\n", i, raw[i]);
+    // ignore first 3 transitions and every uneven number
     if ((i >= 4) && (i%2 == 0)) {
       // shove each bit into the storage bytes
       dht22_dat[j/8] <<= 1;
-      if (counter > 16)
+      if (DEBUG) printf("Counter for bit %d: %d\n", j, raw[i]);
+      if ((raw[i] > 16) && (raw[i] < 200))
         dht22_dat[j/8] |= 1;
       j++;
     }
@@ -75,7 +84,7 @@ static int read_dht22_dat()
 
   // check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
   // print it out if data is good
-  if ((j >= 40) && 
+  if ((j >= 40) &&
       (dht22_dat[4] == ((dht22_dat[0] + dht22_dat[1] + dht22_dat[2] + dht22_dat[3]) & 0xFF)) ) {
         float t, h;
         h = (float)dht22_dat[0] * 256 + (float)dht22_dat[1];
@@ -91,6 +100,8 @@ static int read_dht22_dat()
   else
   {
     printf("Data not good, skip\n");
+    dht_sum=(dht22_dat[0] + dht22_dat[1] + dht22_dat[2] + dht22_dat[3]) & 0xFF;
+    printf("transitions: %d, bitcount: %d, raw: %d %d %d %d, sum %d, checksum %d\n", i, j, dht22_dat[0], dht22_dat[1], dht22_dat[2], dht22_dat[3], dht_sum, dht22_dat[4]);
     return 0;
   }
 }
@@ -105,7 +116,6 @@ int main (int argc, char *argv[])
     printf ("usage: %s <pin> (<tries> <lock>)\ndescription: pin is the wiringPi pin number\nusing 7 (GPIO 4)\nOptional: tries is the number of times to try to obtain a read (default 100)\n          lock: 0 disables the lockfile (for running as non-root user)\n",argv[0]);
   else
     DHTPIN = atoi(argv[1]);
-   
 
   if (argc >= 3)
     tries = atoi(argv[2]);
@@ -124,23 +134,23 @@ int main (int argc, char *argv[])
     exit(EXIT_FAILURE);
   }
 
-  printf ("Raspberry Pi wiringPi DHT22 reader\nwww.lolware.net\n") ;
+  printf ("Raspberry Pi wiringPi DHT22 reader\nwww.lolware.net\namended by gmanic\n") ;
 
   if(lock)
     lockfd = open_lockfile(LOCKFILE);
 
   if (wiringPiSetup () == -1)
     exit(EXIT_FAILURE) ;
-	
+
   if (setuid(getuid()) < 0)
   {
     perror("Dropping privileges failed\n");
     exit(EXIT_FAILURE);
   }
 
-  while (read_dht22_dat() == 0 && tries--) 
+  while (read_dht22_dat() == 0 && tries--)
   {
-     delay(1000); // wait 1sec to refresh
+     delay(1800); // wait 1.8sec to refresh
   }
 
   delay(1500);
